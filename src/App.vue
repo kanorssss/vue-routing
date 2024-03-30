3e65405679fdceab3602c57da446b1079ae54496
Airon Jim <pingadairon@gmail.com>
2024-03-30 12:28:56 +0800

    Folder Structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link :to="{name: 'About'}">About</router-link>
+    <router-link :to="{name: 'About'}">About</router-link> |
+    <router-link :to="{name: 'Jobs'}">Jobs</router-link>
+
   </nav>
   <router-view/>
 </template>
---
+++ src/views/jobs/JobsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+
+    <h2>{{ job.title }}</h2>
+
+</div>
+
+</template>
+
+<script>
+export default {
+        data(){
+            return{
+                jobs: [
+                    {title: 'Ninja UX Designer' , id:1 , details : 'lorem'},
+                    {title: 'Ninja Web Designer' , id:2 , details : 'lorem'},
+                    {title: 'Ninja Vue Designer' , id:3 , details : 'lorem'}
+                ]
+            }
+        }
+}
+</script>
+
+<style>
+
+</style>
